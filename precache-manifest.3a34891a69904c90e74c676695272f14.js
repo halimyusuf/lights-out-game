47a90b880764c99668b9a828584a7d8630cef3a4
halim <haleemyoosuph@gmail.com>
2020-04-03 02:05:45 +0100

    Updates

---
+++ precache-manifest.3a34891a69904c90e74c676695272f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5111dc3bb6ae2e907ad1db1a5fdf1f",
+    "url": "/lights-out-game/index.html"
+  },
+  {
+    "revision": "5ff3a0cd0dbccb0e97d9",
+    "url": "/lights-out-game/static/css/main.9388e8fe.chunk.css"
+  },
+  {
+    "revision": "0cd2ceb4f57109d522da",
+    "url": "/lights-out-game/static/js/2.fd505ea7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lights-out-game/static/js/2.fd505ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff3a0cd0dbccb0e97d9",
+    "url": "/lights-out-game/static/js/main.23a03009.chunk.js"
+  },
+  {
+    "revision": "066ec10385b8b96dd5cc",
+    "url": "/lights-out-game/static/js/runtime-main.e343bdd2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.876ca7130b58e470b5379e2572b6b981.js"
+  "/lights-out-game/precache-manifest.3a34891a69904c90e74c676695272f14.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lights-out-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
